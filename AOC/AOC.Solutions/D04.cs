62a3430499dfc0d11b2508938061d6d8ad491fd2
rodeknopje <51360493+rodeknopje@users.noreply.github.com>
2023-12-04 02:10:28 -0800

    D04

---
+++ AOC/AOC.Solutions/D04.cs
@@ -0,0 +1,57 @@
+﻿namespace AOC.Solutions;
+
+public class D04 : DayBase
+{
+    protected override int Day => 4;
+    
+    public override long Solve_1()
+    {
+        var sum = 0;
+        
+        foreach (var line in GetInputLines())
+        {
+            var nums1 = line.Split("|").First().Split(":").Last().Trim().Split(" ").Where(x => x != "");
+            var nums2 = line.Split("|").Last().Trim().Split(" ").Where(x => x != "");
+
+            var x = nums1.Intersect(nums2).ToArray();
+
+            if (x.Any())
+            {
+                sum += (int)Math.Pow(2, x.Length - 1);
+            }
+        }
+
+        return sum;
+    }
+
+    public override long Solve_2()
+    {
+        var sum = 0;
+
+        var lines = GetInputLines();
+
+        var cards = new long[lines.Count];
+        
+        Array.Fill(cards,1);
+        
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var line = lines[i];
+            
+            var nums1 = line.Split("|").First().Split(":").Last().Trim().Split(" ").Where(x => x != "");
+            var nums2 = line.Split("|").Last().Trim().Split(" ").Where(x => x != "");
+
+            var wins = nums1.Intersect(nums2).Count();
+
+            for (var j = 0; j < wins; j++)
+            {
+                if (j < lines.Count)
+                {
+                    cards[i+j+1] += cards[i];
+                }
+            }
+        }
+
+        return cards.Sum();
+    }
+}
--- AOC/AOC.Solutions/Program.cs
+++ AOC/AOC.Solutions/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AOC.Solutions;
 
-Console.WriteLine($"1: {new D03().Solve_1()}");
-Console.WriteLine($"2: {new D03().Solve_2()}");
+Console.WriteLine($"1: {new D04().Solve_1()}");
+Console.WriteLine($"2: {new D04().Solve_2()}");
